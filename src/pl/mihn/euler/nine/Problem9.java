2ca7ac829be38aaaf6ad02fc9a53befd731887a7
mihn <mihnek@gmail.com>
2011-01-15 00:11:26 +0100

    win!

--- src/pl/mihn/euler/nine/Problem9.java
+++ src/pl/mihn/euler/nine/Problem9.java
@@ -2,6 +2,15 @@ package pl.mihn.euler.nine;
 
 public class Problem9 {
 	public static void main(String[] args) {
-
+		for (int a = 0; a <= 1000; a++) {
+			for (int b = a + 1; b <= (1000 - a); b++) {
+				for (int c = b + 1; c <= (1000 - a - b); c++) {
+					if ((a * a) + (b * b) == (c * c) && (a + b + c == 1000)) {
+						System.out.println("yey " + a + " " + b + " " + c + " "
+								+ a * b * c);
+					}
+				}
+			}
+		}
 	}
 }
