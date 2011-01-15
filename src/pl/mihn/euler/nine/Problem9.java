16a14ef06b0a27ba3e52e0df47c9c4f23f9d91aa
mihn <mihnek@gmail.com>
2011-01-15 23:50:31 +0100

    improved

--- src/pl/mihn/euler/nine/Problem9.java
+++ src/pl/mihn/euler/nine/Problem9.java
@@ -3,7 +3,7 @@ package pl.mihn.euler.nine;
 /**
  * A Pythagorean triplet is a set of three natural numbers, a  b  c, for which,
 
-	a2 + b2 = c2
+	a^2 + b^2 = c^2
 	For example, 32 + 42 = 9 + 16 = 25 = 52.
 
 There exists exactly one Pythagorean triplet for which a + b + c = 1000.
@@ -14,10 +14,10 @@ public class Problem9 {
 	public static void main(String[] args) {
 		for (int a = 0; a <= 1000; a++) {
 			for (int b = a + 1; b <= (1000 - a); b++) { // make sure that a < b
-				for (int c = b + 1; c <= (1000 - a - b); c++) { // make sure that b < c
-					if ((a + b + c == 1000) && (a * a) + (b * b) == (c * c)) { // check other conditions - i have idea about faster way 
-						System.out.println("yey " + a + " " + b + " " + c + " " + a * b * c);
-					}
+				int c = 1000 - a - b;
+				if (b < c && Math.pow(a, 2) + Math.pow(b, 2) == Math.pow(c, 2)) { //check if b < c 
+					// and a^2 + b^2 = c^2
+					System.out.println("yey a:" + a + " b:" + b + " c:" + c + " win:" + a * b * c);
 				}
 			}
 		}
